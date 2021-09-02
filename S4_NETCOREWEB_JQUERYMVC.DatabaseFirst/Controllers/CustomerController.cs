521a48df80e16e80ccb972665024db2ca9843f0f
Luis Chang <luis.chang@outlook.com>
2021-09-01 20:15:59 -0500

    CargarListadoCliente AJAX

---
+++ S4_NETCOREWEB_JQUERYMVC.DatabaseFirst/Controllers/CustomerController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using S4_NETCOREWEB_JQUERYMVC.DatabaseFirst.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace S4_NETCOREWEB_JQUERYMVC.DatabaseFirst.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Listado()
+        {
+            var customers = await CustomerRepo.GetCustomersAsync();
+            return PartialView(customers);
+        }
+
+
+
+
+    }
+}
